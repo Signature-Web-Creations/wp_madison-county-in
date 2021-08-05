35de6cd1c25837deec0bc99e70e033522dcc26b3
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-08-05 00:18:37 -0400

    Add footer to drawer

--- components/base/LeftPanel.vue
+++ components/base/LeftPanel.vue
@@ -5,106 +5,117 @@
     left
     width="33.66%"
     height="100vh"
-    class="px-10 pt-5 blackish"
+    class="pt-5 blackish"
   >
-    <nuxt-link
-      class="d-flex text-decoration-none justify-center align-center flex-nowrap mb-5"
-      :to="{ name: 'index' }"
-    >
-      <v-img
-        class="mr-3"
-        max-height="80"
-        max-width="80"
-        src="https://madisoncounty.in.gov/images/recoloredlogo.png"
-      />
-
-      <v-toolbar-title class="mr-16 text-decoration-none lightgrey--text">
-        <strong>Madison County</strong><br />
-        Indiana
-      </v-toolbar-title>
-    </nuxt-link>
-
-    <v-divider></v-divider>
-
-    <v-sheet class="blackish">
-      <v-card-title
-        class="lightgrey--text font-condensed text-h4 text-uppercase"
+    <div class="px-10">
+      <nuxt-link
+        class="d-flex text-decoration-none justify-center align-center flex-nowrap mb-5"
+        :to="{ name: 'index' }"
       >
-        Office Contact
-      </v-card-title>
+        <v-img
+          class="mr-3"
+          max-height="80"
+          max-width="80"
+          src="https://madisoncounty.in.gov/images/recoloredlogo.png"
+        />
 
-      <v-list flat>
-        <v-list-item class="lightgrey--text">
-          <v-list-item-icon>
-            <v-icon dense class="fa-fw lightgrey--text">
-              fa-user-circle
-            </v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title
-              class="lightgrey--text"
-              v-text="'Office Administrator'"
-            />
-          </v-list-item-content>
-        </v-list-item>
+        <v-toolbar-title class="mr-16 text-decoration-none lightgrey--text">
+          <strong>Madison County</strong><br />
+          Indiana
+        </v-toolbar-title>
+      </nuxt-link>
 
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon dense class="fa-fw lightgrey--text">
-              fa-phone
-            </v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title class="lightgrey--text" v-text="'1231234567'" />
-          </v-list-item-content>
-        </v-list-item>
+      <v-divider></v-divider>
 
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon dense class="fa-fw lightgrey--text">
-              fa-envelope
-            </v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title
-              class="lightgrey--text"
-              v-text="'noreply@madisoncounty.in.gov'"
-            />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-sheet>
+      <v-sheet class="blackish">
+        <v-card-title
+          class="lightgrey--text font-condensed text-h4 text-uppercase"
+        >
+          Office Contact
+        </v-card-title>
 
-    <v-sheet class="blackish mt-2" v-if="resources">
-      <v-card-title
-        class="lightgrey--text font-condensed text-h4 text-uppercase"
-      >
-        Resources
-      </v-card-title>
-
-      <v-list>
-        <v-list-item-group>
-          <v-list-item
-            v-for="item in resources"
-            :key="item.id"
-            :href="item.document.url"
-            target="_blank"
-          >
+        <v-list flat>
+          <v-list-item class="lightgrey--text">
             <v-list-item-icon>
               <v-icon dense class="fa-fw lightgrey--text">
-                fa-file
+                fa-user-circle
               </v-icon>
             </v-list-item-icon>
             <v-list-item-content>
               <v-list-item-title
                 class="lightgrey--text"
-                v-html="item.document.title"
+                v-text="'Office Administrator'"
               />
             </v-list-item-content>
           </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-sheet>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon dense class="fa-fw lightgrey--text">
+                fa-phone
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title
+                class="lightgrey--text"
+                v-text="'1231234567'"
+              />
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon dense class="fa-fw lightgrey--text">
+                fa-envelope
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title
+                class="lightgrey--text"
+                v-text="'noreply@madisoncounty.in.gov'"
+              />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-sheet>
+
+      <v-sheet class="blackish mt-2" v-if="resources">
+        <v-card-title
+          class="lightgrey--text font-condensed text-h4 text-uppercase"
+        >
+          Resources
+        </v-card-title>
+
+        <v-list>
+          <v-list-item-group>
+            <v-list-item
+              v-for="item in resources"
+              :key="item.id"
+              :href="item.document.url"
+              target="_blank"
+            >
+              <v-list-item-icon>
+                <v-icon dense class="fa-fw lightgrey--text">
+                  fa-file
+                </v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  class="lightgrey--text"
+                  v-html="item.document.title"
+                />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-sheet>
+    </div>
+
+    <template v-slot:append>
+      <v-container fluid class="py-2 text-center footer-bottom">
+        © Copyright 2017 Madison County Government
+      </v-container>
+    </template>
   </v-navigation-drawer>
 </template>
 
@@ -126,5 +137,9 @@ export default {
 }
 .font-condensed {
   font-family: $font-condensed !important;
+}
+.footer-bottom {
+  background-color: $color__background-button;
+  color: $color__text-light;
 }
 </style>
