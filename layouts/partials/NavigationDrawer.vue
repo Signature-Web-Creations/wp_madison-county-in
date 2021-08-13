ce2115dfe85c6275c78dd3c9840adea3062d2a98
Kmcgrego <Kmcgrego@users.noreply.github.com>
2021-08-13 10:14:47 -0400

    Add Communties to Navigation

--- layouts/partials/NavigationDrawer.vue
+++ layouts/partials/NavigationDrawer.vue
@@ -62,49 +62,41 @@
           <v-list-item-title>{{ item.name }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
+      <v-list-group no-action sub-group>
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title>City/Township</v-list-item-title>
+          </v-list-item-content>
+        </template>
 
-      <!-- <v-list-item link href="/#directory">
-        <v-list-item-icon>
-          <v-icon>mdi-home</v-icon>
-        </v-list-item-icon>
+        <v-list-item
+          v-for="city in communityList"
+          :key="city.name"
+          link
+          :to="(city.url + '/' + city.name) | lowercase"
+        >
+          <!-- <v-list-item-icon>
+          <v-icon dense>{{ item.icon }}</v-icon>
+        </v-list-item-icon> -->
 
-        <v-list-item-content>
-          <v-list-item-title>Directory</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item> -->
-
-      <!-- <v-list-item link href="/#events">
-        <v-list-item-icon>
-          <v-icon>mdi-home</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>Events</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-list-item link href="/#destinations">
-        <v-list-item-icon>
-          <v-icon>mdi-home</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>Destinations</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item> -->
+          <v-list-item-content>
+            <v-list-item-title>{{ city.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState, mapActions } from "vuex"
 
 export default {
   props: {
     location: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
 
   data() {
@@ -113,56 +105,148 @@ export default {
         {
           name: "Government",
           url: "/#government",
-          icon: "fas fa-university"
+          icon: "fas fa-university",
         },
         {
           name: "Business",
           url: "/#business",
-          icon: "fas fa-handshake"
+          icon: "fas fa-handshake",
         },
         {
           name: "Residents",
           url: "/#residents",
-          icon: "fas fa-house-user"
+          icon: "fas fa-house-user",
         },
         {
           name: "Visitors",
           url: "/#visitors",
-          icon: "fas fa-map-marked-alt"
-        }
+          icon: "fas fa-map-marked-alt",
+        },
       ],
       wuSections: [
         {
           name: "Events",
           url: "/#events",
-          icon: "fas fa-calendar-alt"
+          icon: "fas fa-calendar-alt",
         },
         {
           name: "Directory",
           url: "/directory",
-          icon: "fas fa-folder-open"
+          icon: "fas fa-folder-open",
         },
         {
           name: "Destinations",
           url: "/#destinations",
-          icon: "fas fa-map-signs"
+          icon: "fas fa-map-signs",
+        },
+      ],
+      communityList: [
+        { name: "Alexandria", url: "/community" },
+        { name: "Anderson", url: "/community" },
+        { name: "Chesterfield", url: "/community" },
+        { name: "Edgewoo", url: "/community" },
+        { name: "Elwood", url: "/community" },
+        { name: "Frankton", url: "/community" },
+        { name: "Ingalls", url: "/community" },
+        { name: "Lapel", url: "/community" },
+        { name: "Markleville", url: "/community" },
+        { name: "Orestes", url: "/community" },
+        { name: "Pendleton", url: "/community" },
+        { name: "Summitville", url: "/community" },
+      ],
+    }
+  },
+  async fetch({ store }) {
+    let options = {
+      limit: "500",
+    }
+    await store.dispatch("wuapi/getDirectory", options)
+  },
+  computed: {
+    ...mapState({
+      drawer: (state) => state.navigation.drawer,
+      categories: (state) => state.categories,
+      organizationList: (state) => state.wuapi.directory,
+    }),
+
+    cityItems() {
+      let array = []
+      this.cityList.forEach((element, index) => {
+        let item = {
+          name: element,
+          id: index,
+          children: [],
         }
-      ]
-    };
+        array.push(item)
+      })
+      return array
+    },
   },
 
-  computed: mapState({
-    drawer: state => state.navigation.drawer,
-    categories: state => state.categories
-  }),
-
   methods: {
-    categoryIcon(item) {
-      return this.cat_icon[item];
+    async fetchOrganizations(item) {
+      // Remove in 6 months and say
+      // you've made optimizations! :)
+      await pause(1500)
+
+      if (this.selectedFilter == 1) {
+        this.organizationList.filter((organization) => {
+          organization.categories.forEach((category) => {
+            if (category.name === item.name) {
+              item.children.push(organization)
+            }
+          })
+        })
+      } else if (this.selectedFilter == 2) {
+        this.organizationList.filter((organization) => {
+          if (organization.city === item.name) {
+            item.children.push(organization)
+          }
+        })
+      }
+
+      return item.children
     },
-    ...mapActions("navigation", ["updateDrawer"])
-  }
-};
+    getCityList() {
+      let array = []
+
+      this.organizationList.forEach((element) => {
+        if (element.city != "") {
+          let city = element.city.toLowerCase()
+          city = this.capitalizeWords(city)
+          array.push(city)
+        }
+      })
+
+      array.sort((a, b) => {
+        return a.localeCompare(b)
+      })
+
+      this.cityList = new Set(array)
+    },
+    capitalizeWords(string) {
+      return string.replace(/(?:^|\s)\S/g, function (a) {
+        return a.toUpperCase()
+      })
+    },
+
+    categoryIcon(item) {
+      return this.cat_icon[item]
+    },
+
+    ...mapActions("navigation", ["updateDrawer"]),
+  },
+  created() {
+    this.getCityList()
+  },
+  filters: {
+    lowercase: function (value) {
+      if (!value) return ""
+      value = value.toString()
+      return value.toLowerCase()
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
