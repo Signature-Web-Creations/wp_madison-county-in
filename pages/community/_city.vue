e5c9f22c543fea4a7cde4acd963b40409698c229
Kmcgrego <Kmcgrego@users.noreply.github.com>
2021-08-16 11:32:02 -0400

    Remove comments

--- pages/community/_city.vue
+++ pages/community/_city.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <!-- {{ community }} -->
-    <!-- {{ communities }}
-     -->
     <BaseLeftPanel :resources="community.acf.resources" />
     <CommunityNavigation
       :tabs="tabs"
