f41a47e8d417fe9795a5c92ed03ce3389b71b1e1
Kmcgrego <Kmcgrego@users.noreply.github.com>
2022-01-06 12:54:47 -0500

    Add redirect to external

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,7 +1,8 @@
 // this is a redirect file
-export default function(ctx) {
-  if(ctx.route.fullPath.match(/(gis)(\/)?/i))(
-    ctx.redirect(301, '/government/human-resources')
+export default ({route, redirect, error}) => {
+  console.log(error(500));
+  if(route.fullPath.match(/(gis)(\/)?/i))(
+    redirect("https://www.arcgis.com/apps/webappviewer/index.html?id=09bf2f0e07a243ffb262793cf106cd01")
   )
 }
 
