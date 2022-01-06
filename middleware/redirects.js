45af82da362405d96ef6a3f4153efbeece4a2eab
Kmcgrego <Kmcgrego@users.noreply.github.com>
2022-01-06 13:03:27 -0500

    bug fix

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,6 +1,5 @@
 // this is a redirect file
 export default ({route, redirect, error}) => {
-  console.log(error(500));
   if(route.fullPath.match(/(gis)(\/)?/i))(
     redirect("https://www.arcgis.com/apps/webappviewer/index.html?id=09bf2f0e07a243ffb262793cf106cd01")
   )
